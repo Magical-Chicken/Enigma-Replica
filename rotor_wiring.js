5deaa44bc44eda6fa884390f6004cf8682a638b0
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2015-05-01 12:49:47 -0500

    Added rotor wiring

--- rotor_wiring.js
+++ rotor_wiring.js
@@ -1,12 +1,13 @@
+// Rotor wiring from German Railway (Rocket) [https://en.wikipedia.org/wiki/Enigma_rotor_details]
 var rotor_wirings = [
     // Rotor 1
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "JGDQOXUSCAMIFRVTPNEWKBLZYH",
     // Rotor 2
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "NTZPSFBOKMWRCJDIVLAEYUXHGQ",
     // Rotor 3
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "JVIUBHTCDYAKEQZPOSGXNRMWFL"
 ];
 
-var rotor_reflector = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var rotor_reflector = "QYHOGNECVPUZTFDJAXWMKISRBL";
 
 var rotor_advancements = [1, 26, 676];
